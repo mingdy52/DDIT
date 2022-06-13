5ea664f50ffffa2643556beb1bf5d55362e4efed
mingdy52 <mincount552@gmail.com>
2022-06-13 19:47:14 +0900

    day12 day13

---
+++ Hello_Python/day13/myflask_emp.py
@@ -0,0 +1,79 @@
+from flask import Flask
+import pymysql
+from flask.templating import render_template
+from flask.globals import request
+from conda_build.api import debug
+from flask.json import jsonify
+from day13.dao_emp import DaoEmp
+from flask_cors.extension import CORS
+
+app = Flask(__name__, static_url_path='')
+CORS(app)
+de = DaoEmp()
+
+@app.route('/')
+@app.route('/emp')
+def emp():
+    return render_template('emp.html')
+
+@app.route('/emp.ajax', methods=['POST'])
+def emp_ajax():
+    data = request.get_json()
+    print("data",data['e_id'])
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route('/emp_list.ajax', methods=['POST'])
+def emp_list_ajax():
+    list = de.myselects()
+    return jsonify(list)
+
+@app.route('/emp_one.ajax', methods=['POST'])
+def emp_one_ajax():
+    data = request.get_json()
+    e_id = data['e_id']
+    emp = de.myselect(e_id)
+    
+    return jsonify(emp)
+
+@app.route('/emp_add.ajax', methods=['POST'])
+def emp_add_ajax():
+    data = request.get_json()
+    e_id = data['e_id']
+    e_name = data['e_name']
+    sex = data['sex']
+    addr = data['addr']
+    
+    cnt = -1
+    try:
+        cnt = de.myinsert(e_id, e_name, sex, addr)
+    except:
+        print("insert : error")
+    return jsonify({'cnt':cnt})
+
+@app.route('/emp_mod.ajax', methods=['POST'])
+def emp_mod_ajax():
+    data = request.get_json()
+    e_id = data['e_id']
+    e_name = data['e_name']
+    sex = data['sex']
+    addr = data['addr']
+    cnt = -1
+    try:
+        cnt = de.myupdate(e_id, e_name, sex, addr)
+    except:
+        print("update : error")
+    return jsonify({'cnt':cnt})
+
+@app.route('/emp_del.ajax', methods=['POST'])
+def emp_del_ajax():
+    data = request.get_json()
+    e_id = data['e_id']
+    cnt = -1
+    try:
+        cnt = de.mydelete(e_id)
+    except:
+        print("delete : error")
+    return jsonify({'cnt':cnt})
+
+if __name__ == '__main__':
+    app.run(debug=True)
